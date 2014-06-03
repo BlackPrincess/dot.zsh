02b8a279cb9aa15e953d3417a770dba8e23b57e9
BlackPrincess <black.princess.w@gmail.com>
2014-06-03 12:28:48 +0900

    Changed parameters

--- conf.d/history.sh
+++ conf.d/history.sh
@@ -1,5 +1,5 @@
 HISTFILE=~/.zsh_history
-HISTSIZE=1000000
-SAVEHIST=1000000
+HISTSIZE=10000
+SAVEHIST=10000
 setopt hist_ignore_dups   # ignore duplication command history list
 setopt share_history    # share command history data
